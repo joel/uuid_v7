01af0d6409306220cb4581608f73459f7a181a05
Joel Azemar <joel.azemar@gmail.com>
2023-11-28 13:54:36 +0100

    Change Spec

--- spec/uuid_v7_spec.rb
+++ spec/uuid_v7_spec.rb
@@ -1,11 +1,1 @@
-# frozen_string_literal: true
-
-RSpec.describe UuidV7 do
-  it "has a version number" do
-    expect(UuidV7::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
+RSpec.describe UuidV7
