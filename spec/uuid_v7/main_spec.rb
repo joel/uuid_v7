49510c6a73ac6209a1bd00f327c59eade4cb875e
Joel Azemar <joel.azemar@gmail.com>
2023-11-28 13:54:36 +0100

    Add Main Spec

---
+++ spec/uuid_v7/main_spec.rb
@@ -0,0 +1,14 @@
+module UuidV7
+  RSpec.describe Main do
+    subject(:foo) { described_class.new }
+    describe '#uuid_v7' do
+      context 'when all is good' do
+        let(:input) { 'foo' }
+        let(:result) { 'foo' }
+        it do
+          expect(foo.uuid_v7(input)).to eql(result)
+        end
+      end
+    end
+  end
+end
