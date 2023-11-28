b8f7d3163a50e652d44f91e901f6b7126a8b2d10
Joel Azemar <joel.azemar@gmail.com>
2023-11-28 14:47:24 +0100

    Initial Implementation

--- lib/uuid_v7/convertor.rb
+++ lib/uuid_v7/convertor.rb
@@ -5,15 +5,17 @@ require "base58"
 module UuidV7
   class Convertor
     class << self
-      def to_base58(uuid)
-        raise ArgumentError, "Invalid UUID" unless uuid.match?(/\A\h{8}-\h{4}-\h{4}-\h{4}-\h{12}\z/)
-
-        uuid_to_int = uuid.delete("-").to_i(16)
-
-        Base58.int_to_base58(uuid_to_int)
+      def uuid_to_int(uuid)
+        uuid.delete("-").to_i(16)
       end
 
-      def to_int(base58)
+      def uuid_to_base58(uuid)
+        raise ArgumentError, "Invalid UUID" unless uuid.match?(/\A\h{8}-\h{4}-\h{4}-\h{4}-\h{12}\z/)
+
+        Base58.int_to_base58(uuid_to_int(uuid))
+      end
+
+      def base58_to_int(base58)
         raise ArgumentError, "Invalid Base58" unless base58.match?(/\A\w{21}\z/)
 
         Base58.base58_to_int(base58)
--- spec/uuid_v7/convertor_spec.rb
+++ spec/uuid_v7/convertor_spec.rb
@@ -4,13 +4,18 @@ module UuidV7
   RSpec.describe Convertor do
     let(:uuid)   { "018c160f-5adc-79ad-932b-3d25a62de3d1" }
     let(:base58) { "c5HpnewpE1DtZgL7tir1S" }
+    let(:int)    { 2_056_596_985_533_698_964_084_530_851_032_720_337 }
 
-    it "converts to base58" do
-      expect(described_class.to_base58(uuid)).to match(/\w{21}/)
+    it "converts to int" do
+      expect(described_class.uuid_to_int(uuid)).to eql(int)
     end
 
-    it "converts to base58" do
-      expect(described_class.to_base58(uuid)).to match(/\w{21}/)
-    end
+    # it "converts to base58" do
+    #   expect(described_class.uuid_to_base58(uuid)).to match(/\w{21}/)
+    # end
+
+    # it "converts to base58" do
+    #   expect(described_class.base58_to_int(base58)).to match(/\w{21}/)
+    # end
   end
 end
